8388c3ebaef19ef9334200d8e14ff215d30d496c
RaresDrg <dragan_rares13@yahoo.com>
2023-11-06 06:58:33 +0200

    ex 2, ex 3 partial
    ex 2, ex 3 partial

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+const emailInput = document.querySelector('[name="email"]');
+const messageInput = document.querySelector('[name="message"]');
 
+const serializedData = {
+  data: {
+    email: '',
+    message: '',
+  },
+
+  sentDataLocal() {
+    dataForSentJSON = JSON.stringify(this.data);
+    localStorage.setItem('feedback-form-state', dataForSentJSON);
+  },
+
+  receiveDataLocal() {
+    dataReceivedJSON = localStorage.getItem('feedback-form-state');
+    // dataReceivedObj = JSON.parse(dataReceivedJSON);
+    try {
+      dataReceivedObj = JSON.parse(dataReceivedJSON);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // console.log(dataReceivedObj);
+    return dataReceivedObj;
+  },
+};
+
+// Input Events //
+
+emailInput.addEventListener('input', ({ target }) => {
+  serializedData.data.email = target.value;
+  serializedData.sentDataLocal();
+});
+
+messageInput.addEventListener('input', ({ target }) => {
+  serializedData.data.message = target.value;
+  serializedData.sentDataLocal();
+});
+
+// Page reload event //
+
+window.addEventListener('DOMContentLoaded', fillInputFields);
+
+function fillInputFields() {
+  const dataSaved = serializedData.receiveDataLocal();
+
+  if (dataSaved.email) {
+    emailInput.value = dataSaved.email;
+  }
+
+  if (dataSaved.message) {
+    messageInput.value = dataSaved.message;
+  }
+}
