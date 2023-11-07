9c98267d65d8ad9c284c13b7168c2ea91fefd75d
RaresDrg <dragan_rares13@yahoo.com>
2023-11-07 04:37:22 +0200

    goit-js-hw-08
    goit-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,21 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
 
-function onTimeUpdate(data) {
-  const currentTime = data.seconds;
-  localStorage.setItem('videoplayer-current-time', currentTime);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,52 +5,35 @@ const messageInput = document.querySelector('[name="message"]');
 const formEl = document.querySelector('.feedback-form');
 
 
-// Serialized Data obj //
-const serializedData = {
-  data: {
-    email: emailInput.value,
-    message: messageInput.value,
-  },
-
-  sendDataLocal() {
-    const dataForSentJSON = JSON.stringify(this.data);
-    localStorage.setItem('feedback-form-state', dataForSentJSON);
-  },
-
-  receiveDataLocal() {
-    const dataReceivedJSON = localStorage.getItem('feedback-form-state');
-    const dataReceivedObj = JSON.parse(dataReceivedJSON);
-    return dataReceivedObj;
-  },
-};
-
-
-// Input Events //
+// Input Event //
 formEl.addEventListener(
   'input',
-  throttle(({ target }) => {
-    if (target === emailInput) {
-      serializedData.data.email = target.value;
-      serializedData.sendDataLocal();
-      return;
-    }
-    if (target === messageInput) {
-      serializedData.data.message = target.value;
-      serializedData.sendDataLocal();
-      return;
-    }
+  throttle(() => {
+    const dataObj = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(dataObj));
   }, 500)
 );
 
 
 // Reload page event //
 window.addEventListener('DOMContentLoaded', () => {
-  const dataSaved = serializedData.receiveDataLocal();
+  const dataSaved = receiveDataFromLocal();
   if (dataSaved) {
     emailInput.value = dataSaved.email;
     messageInput.value = dataSaved.message;
   }
 });
+
+// Receive data function //
+function receiveDataFromLocal() {
+  const dataReceivedJSON = localStorage.getItem('feedback-form-state');
+  const dataReceivedObj = JSON.parse(dataReceivedJSON);
+  return dataReceivedObj;
+}
 
 
 // Submit Event //
@@ -67,7 +50,7 @@ formEl.addEventListener('submit', event => {
   }
 
   alert('Formularul a fost trimis');
-  console.log(serializedData.receiveDataLocal());
+  console.log(receiveDataFromLocal());
   localStorage.removeItem('feedback-form-state');
   formEl.reset();
 });
